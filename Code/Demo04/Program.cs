6aab5fa70b4f4e146788d1882ab557e4f3621b9a
Pirjo Mure <n5589@student.jamk.fi>
2019-09-06 12:22:25 +0300

    Addes some code to Main

---
+++ Code/Demo04/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Demo04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("You are better than awesome!");
+        }
+    }
+}
