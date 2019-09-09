61f1aa62e193bc01bc9af6c6746f3ff7a8bfd5ec
Pirjo Mure <n5589@student.jamk.fi>
2019-09-09 13:16:27 +0300

    lisää lab05

---
+++ lab05/Lab05/L05T01/L05T01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace L05T01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Here starts basics of programming with C#");
+            Console.WriteLine("Pirjo Mure");
+            Console.ReadKey();
+           
+        }
+    }
+}
---
+++ lab05/Lab05/Lab05/Lab05/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lab05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Here starts basics of programming with C#");
+            Console.WriteLine("Pirjo Mure");
+
+            Console.ReadKey();
+        }
+    }
+}
